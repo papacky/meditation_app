ba400cc0a7b0c8c5fcfc6974a2224314c183bdbc
papacky <ytpacky@yahoo.co.jp>
2025-07-11 14:17:32 +0900

    瞑想音楽ライブラリの修正

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -5,10 +5,18 @@ class MusicController < ApplicationController
     # public/music/配下の音楽ファイル一覧を取得
     music_dir = Rails.root.join('public', 'music')
     @music_files = []
+    durations = {
+      '01' => '1分', '02' => '21分', '03' => '57分', '04' => '43分', '05' => '47分',
+      '06' => '46分', '07' => '17分', '08' => '6分', '09' => '8分', '10' => '23分',
+      '11' => '45分', '12' => '37分', '13' => '30分', '14' => '9分'
+    }
     if Dir.exist?(music_dir)
-      @music_files = Dir.entries(music_dir).select { |f| f =~ /\.(mp3|wav|flac|m4a|aac|ogg)$/i }
-      # ファイル名先頭の2桁番号でソート
-      @music_files = @music_files.sort_by { |name| name[/\A(\d{2})_/, 1].to_i }
+      files = Dir.entries(music_dir).select { |f| f =~ /\.(mp3|wav|flac|m4a|aac|ogg)$/i }
+      files = files.sort_by { |name| name[/\A(\d{2})_/, 1].to_i }
+      @music_files = files.map do |name|
+        num = name[/\A(\d{2})_/, 1]
+        { name: name, duration: durations[num] || '' }
+      end
     end
   end
 
