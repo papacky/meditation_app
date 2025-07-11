50aeda86b6e7873cfe93c88379331947704463cb
papacky <ytpacky@yahoo.co.jp>
2025-07-11 17:18:15 +0900

    Render NG対応

--- db/migrate/20250704083540_add_file_id_and_file_name_to_meditation_records.rb
+++ db/migrate/20250704083540_add_file_id_and_file_name_to_meditation_records.rb
@@ -1,7 +1,8 @@
 class AddFileIdAndFileNameToMeditationRecords < ActiveRecord::Migration[7.1]
   def change
     add_column :meditation_records, :file_id, :string
-    add_column :meditation_records, :file_name, :string
+    # file_nameカラムは既に存在するため削除
+    # add_column :meditation_records, :file_name, :string
   end
 end
 
